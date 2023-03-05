e5d73c20cdc13a17d7c66a84ac8de1c63832e081
Levin <levin@uncu.de>
2023-03-05 22:38:22 +0100

    Row Wrappers

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,3 +2,5 @@
 
 $GLOBALS['TL_WRAPPERS']['start'][] = 'content_box_start';
 $GLOBALS['TL_WRAPPERS']['stop'][] = 'content_box_stop';
+$GLOBALS['TL_WRAPPERS']['start'][] = 'row_start';
+$GLOBALS['TL_WRAPPERS']['stop'][] = 'row_stop';
