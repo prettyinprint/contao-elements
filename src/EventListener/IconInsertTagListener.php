b3c02c031b4d7c076d984988497b92a32739faa0
Levin <levin@uncu.de>
2023-03-09 22:02:26 +0100

    Fixed Icons Replacement

--- src/EventListener/IconInsertTagListener.php
+++ src/EventListener/IconInsertTagListener.php
@@ -20,8 +20,8 @@ class IconInsertTagListener
   {
     $icons = [
       'arrowDown' => '<path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3" />',
-      'arrowUp' => '<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />',
-      'chevronDown' => '<path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />',
+      'arrowUp' => '<path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />',
+      'chevronDown' => '<path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />',
       'chevronUp' => '<path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />'
     ];
 
@@ -31,7 +31,7 @@ class IconInsertTagListener
     }
 
     $replacement = '<svg xmlns="http://www.w3.org/2000/svg" class="icon">';
-    $replacement .= $icons['arrowDown'];
+    $replacement .= $icons[$iconTarget];
     $replacement .= '</svg>';
 
     return $replacement;
